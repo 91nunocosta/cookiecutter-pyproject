42bd6818d9d5efca512376f964656ae89e5ed708
Nuno Costa <91nunocosta@gmail.com>
2021-11-10 21:41:08 +0000

    feat: Add separated repo, package and module names definitions

--- {{cookiecutter.repo_name}}/tests/test_python_package.py
+++ {{cookiecutter.repo_name}}/tests/test_python_package.py
@@ -2,4 +2,4 @@ from python_package import __version__
 
 
 def test_version():
-    assert __version__ == "{{cookiecutter.project_version}}"
+    assert __version__ == "{{cookiecutter.package_version}}"
