cd4b4822f222a41176e25a167f701854f9454eb8
91nunocosta <91nunocosta@gmail.com>
2023-02-10 12:43:16 +0000

    test: improve tests

--- tests.py
+++ tests.py
@@ -22,9 +22,16 @@ def test_scaffolding(cookies, extra_context):
     print("scaffold finished!")
 
 
-def test_linting_and_tests(cookies):
-    """Test scaffolding a minimal Python package and running pre-commit and tox."""
-    result = cookies.bake()
+@pytest.mark.parametrize(
+    "extra_context",
+    [
+        {"opensource": "no"},
+    ],
+    ids=["non-opensource"],
+)
+def test_linting(cookies, extra_context):
+    """Test scaffolding a minimal Python package and then linting."""
+    result = cookies.bake(extra_context=extra_context)
     assert result.exit_code == 0
     assert result.exception is None
     assert result.project_path.name == "prototype-python-library"
@@ -37,7 +44,25 @@ def test_linting_and_tests(cookies):
         assert command_result.returncode == 0
 
     run(["git", "init"])
+    run(["poetry", "check"])
     run(["poetry", "install"])
     run(["git", "add", "."])
     run(["poetry", "run", "pre-commit", "run", "--all-files"])
+
+
+def test_testing(cookies):
+    """Test scaffolding a minimal Python package and then testing."""
+    result = cookies.bake()
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_path.name == "prototype-python-library"
+    assert result.project_path.is_dir()
+    print("scaffold finished!")
+
+    def run(command: List[str]):
+        print(" ".join(command))
+        command_result = subprocess.run(command, cwd=result.project_path.absolute())
+        assert command_result.returncode == 0
+
+    run(["poetry", "install"])
     run(["poetry", "run", "tox"])
