685b1cecc118676562225742e05c198865134955
91nunocosta <91nunocosta@gmail.com>
2023-02-11 13:38:28 +0000

    test: remove redundant test steps

--- tests.py
+++ tests.py
@@ -8,28 +8,13 @@ import pytest
 @pytest.mark.parametrize(
     "extra_context",
     [{"opensource": "no"}, {"cli": "no"}],
-    ids=["non-opensource", "without cli"],
-)
-def test_scaffolding(cookies, extra_context):
-    """Test scaffolding a minimal Python package."""
-    result = cookies.bake(extra_context=extra_context)
-    assert result.exit_code == 0
-    assert result.exception is None
-    assert result.project_path.name == "prototype-python-library"
-    assert result.project_path.is_dir()
-    print("scaffold finished!")
-
-
-@pytest.mark.parametrize(
-    "extra_context",
-    [{"opensource": "no"}, {"cli": "no"}],
     ids=[
         "non-opensource",
         "without cli",
     ],
 )
 def test_linting(cookies, extra_context):
-    """Test scaffolding a minimal Python package and then linting."""
+    """Test scaffolding and then linting."""
     result = cookies.bake(extra_context=extra_context)
     assert result.exit_code == 0
     assert result.exception is None
@@ -65,21 +50,4 @@ def test_testing(cookies):
 
     run(["poetry", "install"])
     run(["poetry", "run", "tox"])
-
-
-def test_run_command(cookies):
-    """Test scaffolding the Python package and then running the cli."""
-    result = cookies.bake()
-    assert result.exit_code == 0
-    assert result.exception is None
-    assert result.project_path.name == "prototype-python-library"
-    assert result.project_path.is_dir()
-    print("scaffold finished!")
-
-    def run(command: List[str]):
-        print(" ".join(command))
-        command_result = subprocess.run(command, cwd=result.project_path.absolute())
-        assert command_result.returncode == 0
-
-    run(["poetry", "install"])
     run(["poetry", "run", "prototype-python-library", "10"])
