5f901892bc3e190ffabea6f3cba9f3cf4514489c
91nunocosta <91nunocosta@gmail.com>
2023-04-05 16:08:59 +0100

    fix: fix test_cli docstrings

--- {{cookiecutter.package_name}}/tests/test_cli.py
+++ {{cookiecutter.package_name}}/tests/test_cli.py
@@ -1,4 +1,4 @@
-"""Test the command line interface for prototype-python-library"""
+"""Test the command line interface for {{ cookiecutter.module_name }}."""
 from typer.testing import CliRunner
 
 from {{ cookiecutter.module_name }}.cli import app
@@ -7,7 +7,7 @@ runner = CliRunner()
 
 
 def test_app() -> None:
-    """Test the command line interface for prototype-python-library"""
+    """Test the command line interface for {{ cookiecutter.module_name }}"""
     result = runner.invoke(app, ["10"])
     assert result.exit_code == 0
     assert result.stdout.strip() == "55"
