0038be9db0828ce41ed1ff0364f8f6748966563d
91nunocosta <91nunocosta@gmail.com>
2023-05-07 12:38:22 +0100

    feat: split parameters release and docs

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,9 +5,13 @@ OPENSOURCE_FILES: List[Path] = [
     Path("CODE_OF_CONDUCT.md"),
     Path("CONTRIBUTING.md"),
     Path("LICENSE"),
+]
+RELEASE_FILES: List[Path] = [
     Path(".markdownlintignore"),
     Path(".github/workflows/commitlint.yml"),
     Path(".github/workflows/release_package.yml"),
+]
+DOCS_FILES: List[Path] = [
     Path(".github/workflows/docs.yml"),
 ]
 CLI_FILES: List[Path] = [
@@ -23,4 +27,6 @@ def remove(paths: List[Path]) -> None:
             path.unlink()
 
 {% if cookiecutter.opensource != "yes" %}remove(OPENSOURCE_FILES) {% endif %}
+{% if cookiecutter.docs != "yes" %}remove(DOCS_FILES) {% endif %}
+{% if cookiecutter.release != "yes" %}remove(RELEASE_FILES) {% endif %}
 {% if cookiecutter.cli != "yes" %}remove(CLI_FILES) {% endif %}
